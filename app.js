0aef53aa42c57200383b3c6193ed1efee617319b
sagul-09 <nihal.evil.royal@gmail.com>
2023-06-26 08:35:16 +0530

    impoerted necessery packages

--- app.js
+++ app.js
@@ -2,5 +2,11 @@ import express from "express";
 const app = express();
 import mongoose from "mongoose";
 import cors from "cors";
-import * as dotenv from "dotenv"
+import * as dotenv from "dotenv";
 
+app.get("/", (req, res) => {
+  res.send("working");
+});
+app.listen(3500, () => {
+  console.log(`server is running in http://localhost:3500`);
+});
