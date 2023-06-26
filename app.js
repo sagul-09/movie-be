96008d33a08ca84c3262c62009c3d7cfde844483
sagul-09 <nihal.evil.royal@gmail.com>
2023-06-26 08:50:14 +0530

    added movieModel

--- app.js
+++ app.js
@@ -4,8 +4,21 @@ import mongoose from "mongoose";
 import cors from "cors";
 import * as dotenv from "dotenv";
 
+app.use(express.json());
+app.use(cors());
+
+dotenv.config();
+mongoose.connect(process.env.DB_URL);
+const db = mongoose.connection;
+db.on("error", (errorMessage) => {
+  console.log(errorMessage);
+});
+db.once("open", () => {
+  console.log("Connected successfully to the database!");
+});
+
 app.get("/", (req, res) => {
-  res.send("working");
+  res.send("server is working");
 });
 app.listen(3500, () => {
   console.log(`server is running in http://localhost:3500`);
---
+++ model/movieModel.js
@@ -0,0 +1,62 @@
+const movieSchema = new mongoose.Schema(
+  {
+    movieName: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    movieYear: {
+      type: String,
+      required: true,
+    },
+    movieGenre1: {
+      type: String,
+      enum: [
+        "Action",
+        "Comedy",
+        "Drama",
+        "SciFi",
+        "Horror",
+        "Thriller",
+        "Romance",
+        "Fantasy",
+        "Animation",
+        "Adventure",
+        "Crime",
+        "Biography",
+      ],
+      required: true,
+    },
+    movieGenre2: {
+      type: String,
+      enum: [
+        "Action",
+        "Comedy",
+        "Drama",
+        "SciFi",
+        "Horror",
+        "Thriller",
+        "Romance",
+        "Fantasy",
+        "Animation",
+        "Adventure",
+        "Crime",
+        "Biography",
+      ],
+      required: true,
+    },
+    imdbRating: {
+      type: Number,
+      required: true,
+      min: 0,
+      max: 10,
+    },
+  },
+  {
+    collection: "movies",
+  }
+);
+const movieModel = mongoose.model("Movie", movieSchema);
+
+export default movieModel;
